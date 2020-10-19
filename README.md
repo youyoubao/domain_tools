333f2a7a40bcd6b1645b96116522f221bcf1dc1f
文帅营 <wenshuaiying@gongchang.com>
2020-10-19 14:20:09 +0800

    初步完成相关功能

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# domain_tools
+# 批量查询域名对应的ip、所在地
+
+## 使用方法
+* 下载本项目，通过终端进入项目目录
+* 运行`composer i `，如果提示没有composer，百度搜索安装
+* 编辑domain.txt文件，将需要查询的域名粘贴到文件中
+* 运行`php index.php`，会输出查询结果。可以直接复制粘贴到excel
+
--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-
 // 批量查询域名对应的ip、地区
+
+require 'vendor/autoload.php';
+use itbdw\Ip\IpLocation;
+
+$domain         = file('domain.txt');
+$qqwry_filepath = '/abspath/qqwry.dat';
+foreach ($domain as $value) {
+    $value = trim($value);
+    if (!$value) {
+        continue;
+    }
+    $ip = trim(`nslookup $value |grep 'Address' | tail -n 1 | awk '{print $2}'`);
+
+    $areaInfo = IpLocation::getLocation($ip);
+    echo "\n" . $value . "\t" . implode("\t", $areaInfo);
+}
